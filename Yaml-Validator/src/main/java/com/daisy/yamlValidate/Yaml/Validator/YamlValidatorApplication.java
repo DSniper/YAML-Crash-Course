0a6af1941165a6ea442beb391b8a5c2f49366c7a
Daisy Manmohan Singh <singhdmgangian@gmail.com>
2025-08-27 04:43:31 +0530

    Adding all the files

---
+++ Yaml-Validator/src/main/java/com/daisy/yamlValidate/Yaml/Validator/YamlValidatorApplication.java
@@ -0,0 +1,36 @@
+package com.daisy.yamlValidate.Yaml.Validator;
+
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class YamlValidatorApplication {
+    public static boolean isValidYAML(String fp) {
+        LoaderOptions op = new LoaderOptions();
+        Yaml yaml = new Yaml(new SafeConstructor(op));
+        try (InputStream input = new FileInputStream(fp)) {
+            yaml.load(input);
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+//			throw new RuntimeException(e);
+            System.out.println("Invalid YAML: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = "D:\\Softwares\\YAML Crash Course\\Yaml Validator\\Yaml-Validator\\src\\main\\YAML Learning Files\\one.yaml";
+        if (isValidYAML(path)) {
+            System.out.println(" The File is Valid YAML");
+        } else {
+            System.out.println(" The File is Not a Valid YAML");
+        }
+    }
+}
---
+++ Yaml-Validator/src/test/java/com/daisy/yamlValidate/Yaml/Validator/YamlValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daisy.yamlValidate.Yaml.Validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
